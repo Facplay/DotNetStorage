fd65c33e555ac2e81369fb9df5e794ef08fa89f2
Jose Fabian Cataño <jfcsoluciones@gmail.com>
2019-08-02 18:11:29 -0500

    Creación de proyecto de pruebas unitarias.

---
+++ DotNetStorage.Test/UnitTestStorage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using DotNetStorage.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetStorage.Test
+{
+    [TestClass]
+    public class UnitTestStorage
+    {
+        [TestMethod]
+        public void StorageTest()
+        {
+            try
+            {
+                var config = new StorageConfig()
+                {
+                    BaseDirectory = "C:\\Windows\\Temp",
+                    Filename = ".testStorage"
+                };
+
+                using (var localstorage = new Storage(config))
+                {
+
+                    localstorage.Store("Test", "TestValue", TimeSpan.FromMinutes(60));
+                }
+
+                Assert.IsTrue(true);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void StorageAppConfigTest()
+        {
+            try
+            {
+                using (var localstorage = new Storage())
+                {
+                    localstorage.Store("TestAppConfig", "TestTestAppConfigValue");
+
+                    localstorage.Persist();
+                }
+
+                Assert.IsTrue(true);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
